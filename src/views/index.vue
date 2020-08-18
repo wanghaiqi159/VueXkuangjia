0559ad1bcd114134464fa0ed6dba024f943285e3
924237130@qq.com <924237130@qq.com>
2020-08-18 11:46:49 +0800

    23

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,23 +1,45 @@
 <template>
-<div style="width:100%;height:100%;">
-这里是首页
-    <div style="display:flex;width:100%;height: calc(100% - 25px)">
-        <div style="width:20%;height: 100%;border:1px solid red;">
-        侧边导航
-        </div>
-        <div style="width:80%;height: 100% ;border:1px solid red;">
-            <router-view></router-view>
-        </div>
-    </div>
+<div class=''>
+    
 </div>
-    
-    
 </template>
+
 <script>
+
 export default {
-    
+components: {},
+data() {
+//这里存放数据
+return {
+
+};
+},
+//监听属性 类似于data概念
+computed: {},
+//监控data中的数据变化
+watch: {},
+//方法集合
+methods: {
+
+},
+//生命周期 - 创建完成（可以访问当前this实例）
+created() {
+
+},
+//生命周期 - 挂载完成（可以访问DOM元素）
+mounted() {
+
+},
+beforeCreate() {}, //生命周期 - 创建之前
+beforeMount() {}, //生命周期 - 挂载之前
+beforeUpdate() {}, //生命周期 - 更新之前
+updated() {}, //生命周期 - 更新之后
+beforeDestroy() {}, //生命周期 - 销毁之前
+destroyed() {}, //生命周期 - 销毁完成
+activated() {}, //如果页面有keep-alive缓存功能，这个函数会触发
 }
 </script>
-<style scoped>
+<style lang='scss' scoped>
+//@import url(); 引入公共css类
 
 </style>
